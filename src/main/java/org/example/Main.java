999eca9da2d411f21cf2d9199a74b34e9df9ffd1
Марія-Валерія Гуза <mariia-valeriia.huza@ukma.edu.ua>
2024-02-05 16:29:31 +0200

    Create arrays

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,20 +1,37 @@
 package org.example;
 
+import java.io.*;
+import java.math.BigDecimal;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         new Main().run();
     }
 
-    private void run() {
+    private void run(){
 
-        Scanner in = new Scanner(System.in);
+//        Scanner in = new Scanner(System.in);
+//
+//        double x = in.nextDouble();
+//        //variant 3
+//        double res = calculateFunction(x);
+//        System.out.println(res);
 
-        double x = in.nextDouble();
-        //variant 5
-        double res = calculateFunction(x);
-        System.out.println(res);
+        List<Double> resX = createArrayY(0.0,2.0,0.002);
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("list.txt", StandardCharsets.UTF_8))) {
+            // Write each element of the array to the file
+            for (Double element : resX) {
+                writer.write(String.valueOf(element));
+                writer.newLine(); // Add a newline character between elements if needed
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public double calculateFunction(double x) {
@@ -39,6 +56,36 @@ public class Main {
     }
 
     public int stepsAmount(double start, double end, double step){
-        return (int) Math.ceil((end - start) / step) ;
+        return (int) Math.ceil((end - start) / step)+1 ;
     }
+
+    public List<Double> createArrayX(double start, double end, double step){
+
+        List<Double> res = new ArrayList<>();
+
+        int stepsAmount = stepsAmount(start,end,step);
+
+        BigDecimal startX = BigDecimal.valueOf(start);
+        for (int i = 0; i < stepsAmount; i++){
+            res.add(startX.doubleValue());
+            startX = startX.add(BigDecimal.valueOf(0.002));
+        }
+
+        return res;
+    }
+
+    public List<Double> createArrayY(double start, double end, double step){
+
+        List<Double> res = new ArrayList<>();
+
+        List<Double> arrayX = createArrayX(start,end,step);
+
+       for (double x : arrayX){
+           res.add(calculateFunction(x));
+       }
+
+        return res;
+    }
+
+
 }
